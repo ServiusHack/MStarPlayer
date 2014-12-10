a7d37df427a8e634e69fd06601e4036e6420c218
Severin Leonhardt <phoenixseve@gmx.de>
2014-12-10 20:51:25 +0100

    DarkLookAndFeel derives from DefaultLookAndFeel to share common code.

--- Source/DarkLookAndFeel.h
+++ Source/DarkLookAndFeel.h
@@ -2,13 +2,11 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+#include "DefaultLookAndFeel.h"
+
 class DarkLookAndFeel
-	: public LookAndFeel_V3
+	: public DefaultLookAndFeel
 {
 public:
 	DarkLookAndFeel();
-
-	void drawProgressBar (Graphics& g, ProgressBar& progressBar,
-						  int width, int height,
-						  double progress, const String& textToShow);
 };
