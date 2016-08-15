d69625ef408ca31acb4ec0bda191bbb5baf2a2b2
Severin Leonhardt <phoenixseve@gmx.de>
2016-08-15 23:25:27 +0200

    Copy code from base ChannelRemappingAudioSource
    This copies the required logic from ChannelRemappingAudioSource to ChannelRemappingAudioSourceWithVolume in preparation of supporting two remapped outputs for each output.

--- Source/ChannelRemappingAudioSourceWithVolume.cpp
+++ Source/ChannelRemappingAudioSourceWithVolume.cpp
@@ -4,49 +4,148 @@
 
 ChannelRemappingAudioSourceWithVolume::ChannelRemappingAudioSourceWithVolume(AudioSource* const source_,
                                                           const bool deleteSourceWhenDeleted)
-    : ChannelRemappingAudioSource(source_, deleteSourceWhenDeleted)
-	, m_bufferSize(1)
+   : source (source_, deleteSourceWhenDeleted)
+   ,  requiredNumberOfChannels (2)
+    , m_bufferSize(1)
 {
+    remappedInfo.buffer = &buffer;
+    remappedInfo.startSample = 0;
 }
 
 void ChannelRemappingAudioSourceWithVolume::setNumberOfChannelsToProduce(int requiredNumberOfChannels_)
 {
-	const ScopedLock sl(lock);
-	m_volumes.resize(requiredNumberOfChannels_, VolumeAnalyzer(m_bufferSize));
-	ChannelRemappingAudioSource::setNumberOfChannelsToProduce(requiredNumberOfChannels_);
+    const ScopedLock sl(lock);
+    requiredNumberOfChannels = requiredNumberOfChannels_;
+    m_volumes.resize(requiredNumberOfChannels_, VolumeAnalyzer(m_bufferSize));
+}
+
+void ChannelRemappingAudioSourceWithVolume::clearAllMappings()
+{
+    const ScopedLock sl (lock);
+
+    remappedOutputs.clear();
+}
+
+int ChannelRemappingAudioSourceWithVolume::getRemappedOutputChannel (const int outputChannelIndex) const
+{
+    const ScopedLock sl (lock);
+
+    if (outputChannelIndex >= 0 && outputChannelIndex < remappedOutputs.size())
+        return remappedOutputs[outputChannelIndex];
+
+    return -1;
 }
 
 void ChannelRemappingAudioSourceWithVolume::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
-	ChannelRemappingAudioSource::prepareToPlay(samplesPerBlockExpected, sampleRate);
-	const ScopedLock sl(lock);
-	m_bufferSize = static_cast<size_t>(sampleRate/10);
-	auto numberOfChannels = m_volumes.size();
-	m_volumes.clear();
-	m_volumes.resize(numberOfChannels, VolumeAnalyzer(m_bufferSize));
-	m_sampleRate = sampleRate;
+    source->prepareToPlay (samplesPerBlockExpected, sampleRate);
+
+    const ScopedLock sl(lock);
+    m_bufferSize = static_cast<size_t>(sampleRate/10);
+    auto numberOfChannels = m_volumes.size();
+    m_volumes.clear();
+    m_volumes.resize(numberOfChannels, VolumeAnalyzer(m_bufferSize));
+    m_sampleRate = sampleRate;
+}
+
+void ChannelRemappingAudioSourceWithVolume::releaseResources()
+{
+    source->releaseResources();
 }
 
 void ChannelRemappingAudioSourceWithVolume::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill)
 {
-	const ScopedLock sl(lock);
-	ChannelRemappingAudioSource::getNextAudioBlock(bufferToFill);
-	for (size_t i = 0; i < m_volumes.size(); ++i) {
-		m_volumes[i].update(bufferToFill.buffer->getReadPointer(i) + bufferToFill.startSample, bufferToFill.numSamples);
-	}
+    const ScopedLock sl(lock);
+
+    buffer.setSize (requiredNumberOfChannels, bufferToFill.numSamples, false, false, true);
+
+    const int numChans = bufferToFill.buffer->getNumChannels();
+
+    for (int i = 0; i < buffer.getNumChannels(); ++i)
+    {
+        buffer.clear (i, 0, bufferToFill.numSamples);
+    }
+
+    remappedInfo.numSamples = bufferToFill.numSamples;
+
+    source->getNextAudioBlock (remappedInfo);
+
+    bufferToFill.clearActiveBufferRegion();
+
+    for (int i = 0; i < requiredNumberOfChannels; ++i)
+    {
+        {
+            const int remappedChan = getRemappedOutputChannel (i);
+
+            if (remappedChan >= 0 && remappedChan < numChans)
+            {
+                bufferToFill.buffer->addFrom (remappedChan, bufferToFill.startSample,
+                                              buffer, i, 0, bufferToFill.numSamples);
+
+            }
+        }
+    }
+
+    for (size_t i = 0; i < m_volumes.size(); ++i) {
+        m_volumes[i].update(bufferToFill.buffer->getReadPointer(i) + bufferToFill.startSample, bufferToFill.numSamples);
+    }
+}
+
+XmlElement* ChannelRemappingAudioSourceWithVolume::createXml() const
+{
+    XmlElement* e = new XmlElement ("MAPPINGS");
+    String outs;
+
+    const ScopedLock sl (lock);
+
+    for (int i = 0; i < remappedOutputs.size(); ++i)
+        outs << remappedOutputs[i] << ' ';
+
+    e->setAttribute ("outputs", outs.trimEnd());
+
+    return e;
+}
+
+void ChannelRemappingAudioSourceWithVolume::restoreFromXml (const XmlElement& e)
+{
+    if (e.hasTagName ("MAPPINGS"))
+    {
+        const ScopedLock sl (lock);
+
+        clearAllMappings();
+
+        StringArray outs;
+        outs.addTokens (e.getStringAttribute ("outputs"), false);
+
+        for (int i = 0; i < outs.size(); ++i)
+            remappedOutputs.add (outs[i].getIntValue());
+    }
 }
 
 float ChannelRemappingAudioSourceWithVolume::getVolume() const
 {
-	const ScopedLock sl(lock);
-	float maxVolume = 0;
-	for (auto&& volume : m_volumes)
-		maxVolume = std::max(maxVolume, volume.getVolume());
+    const ScopedLock sl(lock);
+    float maxVolume = 0;
+    for (auto&& volume : m_volumes)
+        maxVolume = std::max(maxVolume, volume.getVolume());
 
-	return maxVolume;
+    return maxVolume;
 }
 
 double ChannelRemappingAudioSourceWithVolume::getSampleRate() const
 {
-	return m_sampleRate;
+    return m_sampleRate;
+}
+
+void ChannelRemappingAudioSourceWithVolume::setOutputChannelMapping(int sourceChannelIndex, int destChannelIndex)
+{
+    const ScopedLock sl (lock);    setOutputChannelMappingInternal(sourceChannelIndex, destChannelIndex);
+}
+
+void ChannelRemappingAudioSourceWithVolume::setOutputChannelMappingInternal(const int sourceIndex, const int destIndex)
+{
+    while (remappedOutputs.size() < sourceIndex+1)
+        remappedOutputs.add(-1);
+
+    remappedOutputs.set(sourceIndex, destIndex);
 }
--- Source/ChannelRemappingAudioSourceWithVolume.h
+++ Source/ChannelRemappingAudioSourceWithVolume.h
@@ -7,7 +7,8 @@
 /**
 	Audio source which remaps channels and tracks the current volume of each channel.
 */
-class ChannelRemappingAudioSourceWithVolume : public ChannelRemappingAudioSource
+class ChannelRemappingAudioSourceWithVolume
+	: public AudioSource
 {
 public:
     ChannelRemappingAudioSourceWithVolume(AudioSource* source, bool deleteSourceWhenDeleted);
@@ -20,14 +21,66 @@ public:
 
 private:
 	std::vector<VolumeAnalyzer> m_volumes;
-	CriticalSection lock;
 	size_t m_bufferSize;
 	double m_sampleRate;
 
 // ChannelRemappingAudioSource
 public:
+    //==============================================================================
+    /** Specifies a number of channels that this audio source must produce from its
+        getNextAudioBlock() callback.
+    */
 	void setNumberOfChannelsToProduce(int requiredNumberOfChannels);
+
+    /** Clears any mapped channels.
+
+        After this, no channels are mapped, so this object will produce silence. Create
+        some mappings with setInputChannelMapping() and setOutputChannelMapping().
+    */
+    void clearAllMappings();
+
+    /** Creates an output channel mapping.
+
+        When the getNextAudioBlock() method is called, the data returned in channel sourceChannelIndex by
+        our input audio source will be copied to channel destChannelIndex of the final buffer.
+
+        @param sourceChannelIndex   the index of an output channel coming from our input audio source
+                                    (i.e. the source specified when this object was created).
+        @param destChannelIndex     the index of the output channel in the incoming audio data buffer
+                                    during our getNextAudioBlock() callback
+    */
+    void setOutputChannelMapping (int sourceChannelIndex,
+                                  int destChannelIndex);
+
+	void setOutputChannelMappingInternal(const int sourceIndex, const int destIndex);
+
+    /** Returns the output channel to which channel outputChannelIndex of our input audio
+        source will be sent to.
+    */
+    int getRemappedOutputChannel (int outputChannelIndex) const;
+
+    //==============================================================================
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void releaseResources() override;
 	virtual void getNextAudioBlock(const AudioSourceChannelInfo&) override;
 
+    //==============================================================================
+    /** Returns an XML object to encapsulate the state of the mappings.
+        @see restoreFromXml
+    */
+    XmlElement* createXml() const;
+
+    /** Restores the mappings from an XML object created by createXML().
+        @see createXml
+    */
+    void restoreFromXml (const XmlElement&);
+
+private:
+    OptionalScopedPointer<AudioSource> source;
+    Array<int> remappedOutputs;
+    int requiredNumberOfChannels;
+
+    AudioSampleBuffer buffer;
+    AudioSourceChannelInfo remappedInfo;
+    CriticalSection lock;
 };
