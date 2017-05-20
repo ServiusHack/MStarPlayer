b7d1899be5d61aa56747e42dbba0a15a3826dc9f
Severin Leonhardt <phoenixseve@gmx.de>
2017-05-20 17:59:25 +0200

    Update to Juce 5.0.1

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -4,8 +4,7 @@
 
 */
 
-#ifndef BINARYDATA_H_24633736_INCLUDED
-#define BINARYDATA_H_24633736_INCLUDED
+#pragma once
 
 namespace BinaryData
 {
@@ -61,5 +60,3 @@ namespace BinaryData
     // return the corresponding data and its size (or a null pointer if the name isn't found).
     const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
 }
-
-#endif
--- JuceLibraryCode/modules/juce_opengl/native/juce_OpenGL_linux_X11.h
+++ JuceLibraryCode/modules/juce_opengl/native/juce_OpenGL_linux_X11.h
@@ -2,27 +2,28 @@
   ==============================================================================
 
    This file is part of the JUCE library.
-   Copyright (c) 2015 - ROLI Ltd.
+   Copyright (c) 2017 - ROLI Ltd.
 
-   Permission is granted to use this software under the terms of either:
-   a) the GPL v2 (or any later version)
-   b) the Affero GPL v3
+   JUCE is an open source library subject to commercial or open-source
+   licensing.
 
-   Details of these licenses can be found at: www.gnu.org/licenses
+   By using JUCE, you agree to the terms of both the JUCE 5 End-User License
+   Agreement and JUCE 5 Privacy Policy (both updated and effective as of the
+   27th April 2017).
 
-   JUCE is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-   A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+   End User License Agreement: www.juce.com/juce-5-licence
+   Privacy Policy: www.juce.com/juce-5-privacy-policy
 
-   ------------------------------------------------------------------------------
+   Or: You may also use this code under the terms of the GPL v3 (see
+   www.gnu.org/licenses).
 
-   To release a closed-source product which uses JUCE, commercial licenses are
-   available: visit www.juce.com for more information.
+   JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
 
   ==============================================================================
 */
 
-extern ::Display* display;
 extern XContext windowHandleXContext;
 
 //==============================================================================
@@ -61,7 +62,9 @@ public:
         : component (comp), renderContext (0), embeddedWindow (0), swapFrames (0), bestVisual (0),
           contextToShareWith (shareContext), context (nullptr), dummy (*this)
     {
-        ScopedXLock xlock;
+        display = XWindowSystem::getInstance()->displayRef();
+
+        ScopedXLock xlock (display);
         XSync (display, False);
 
         GLint attribs[] =
@@ -125,18 +128,20 @@ public:
 
         if (embeddedWindow != 0)
         {
-            ScopedXLock xlock;
+            ScopedXLock xlock (display);
             XUnmapWindow (display, embeddedWindow);
             XDestroyWindow (display, embeddedWindow);
         }
 
         if (bestVisual != nullptr)
             XFree (bestVisual);
+
+        XWindowSystem::getInstance()->displayUnref();
     }
 
     void initialiseOnRenderThread (OpenGLContext& c)
     {
-        ScopedXLock xlock;
+        ScopedXLock xlock (display);
         renderContext = glXCreateContext (display, bestVisual, (GLXContext) contextToShareWith, GL_TRUE);
         c.makeActive();
         context = &c;
@@ -163,6 +168,8 @@ public:
 
     static void deactivateCurrentContext()
     {
+        ScopedXDisplay xDisplay;
+        ::Display* display = xDisplay.get();
         glXMakeCurrent (display, None, 0);
     }
 
@@ -178,7 +185,7 @@ public:
         const Rectangle<int> physicalBounds =
             juce_LinuxScaledToPhysicalBounds (component.getPeer(), bounds);
 
-        ScopedXLock xlock;
+        ScopedXLock xlock (display);
         XMoveResizeWindow (display, embeddedWindow,
                            physicalBounds.getX(), physicalBounds.getY(),
                            (unsigned int) jmax (1, physicalBounds.getWidth()),
@@ -229,12 +236,17 @@ private:
     OpenGLContext* context;
     DummyComponent dummy;
 
+    ::Display* display;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NativeContext)
 };
 
 //==============================================================================
 bool OpenGLHelpers::isContextActive()
 {
-    ScopedXLock xlock;
+    ScopedXDisplay xDisplay;
+    ::Display* display = xDisplay.get();
+
+    ScopedXLock xlock (display);
     return glXGetCurrentContext() != 0;
 }
