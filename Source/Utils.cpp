66fdebdf97862e982fac4f97c57e551d67fab086
Severin Leonhardt <serviushack@gmx.de>
2020-12-14 18:21:33 +0100

    Reduce code duplication
    `isAudioFile` existed twice with the same implementation. Moved to Utils
    and also moved `isImageFile` because it is very similar.

--- Source/Utils.cpp
+++ Source/Utils.cpp
@@ -1,5 +1,7 @@
 #include "Utils.h"
 
+#include "juce_audio_formats/juce_audio_formats.h"
+
 juce::String Utils::formatSeconds(double doubleTotalSeconds)
 {
     const int totalSeconds = static_cast<int>(doubleTotalSeconds);
@@ -8,4 +10,28 @@ juce::String Utils::formatSeconds(double doubleTotalSeconds)
     const int milliseconds = (static_cast<int>(doubleTotalSeconds * 1000)) % 1000;
 
     return juce::String::formatted("%02i:%02i:%03i", minutes, seconds, milliseconds);
+}
+
+bool Utils::isAudioFile(const juce::String& filePath)
+{
+    juce::AudioFormatManager formatManager;
+    formatManager.registerBasicFormats();
+
+    for (int i = 0; i < formatManager.getNumKnownFormats(); ++i)
+    {
+        for (auto&& extension : formatManager.getKnownFormat(i)->getFileExtensions())
+        {
+            if (filePath.endsWithIgnoreCase(extension))
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool Utils::isImageFile(const juce::String& filePath)
+{
+    return filePath.endsWithIgnoreCase(".jpg") || filePath.endsWithIgnoreCase(".png");
 }
--- Source/Utils.h
+++ Source/Utils.h
@@ -11,4 +11,7 @@ public:
     /* Return seconds in the common mm:ss:zzz format.
      */
     static juce::String formatSeconds(double totalSeconds);
+
+    static bool isAudioFile(const juce::String& filePath);
+    static bool isImageFile(const juce::String& filePath);
 };
