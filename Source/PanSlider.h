fbf067fa360cae028f546c4aed45d9efa0dace66
Severin Leonhardt <serviushack@gmx.de>
2025-01-17 16:57:10 +0100

    Support panning stereo player tracks
    Some code for this already existed but was always disabled because the
    `panEnabled` parameter was always set to `false`. This commit now adds
    the remaining changes necessary to be able to pan stereo player tracks.
    
    The gain is applied in `ChannelRemappingAudioSourceWithVolume` similar
    to how the output channel mapping works with an array indexed by the
    source channel number.
    
    The fader is implemented in `PanSlider` as a copy of the `VolumeSlider`
    with different visuals but the same features. Specifically it also snaps
    to a neutral position which is marked with a line.
    
    The existing code placed the slider below the solo and mute buttons,
    squeezing the volume slider but leaving an empty label above. This isn't
    good use of the available space. Instead the pan slider now replaces the
    label for tracks.
    
    The pan slider works in the value range -1.0 to 1.0 from which the gain
    values for both channels are calculated in `Track:setPan`.

---
+++ Source/PanSlider.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "juce_gui_basics/juce_gui_basics.h"
+
+/* Slider to change a volume.
+ *
+ * Also called fader. It has a logarithmic scale with 0 dB at about 80% to which it snaps.
+ * The slider ranges from -inf dB to +10 dB.
+ */
+class PanSlider : public juce::Slider
+{
+public:
+    PanSlider();
+
+protected:
+    // Distance in gain from 0 when to snap to 0.
+    double m_snapDistance;
+
+    // Slider
+public:
+    virtual void paint(juce::Graphics&) override;
+    virtual double snapValue(double attemptedValue, juce::Slider::DragMode dragMode) override;
+};
+
