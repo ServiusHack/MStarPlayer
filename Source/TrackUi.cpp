ServiusHack/MStarPlayer

commit 45bf80fca860bf4c16b244ae97b3165f934a7d8e

Update to JUCE 6.1.0
All code changes are related to this breaking change in JUCE:

> The default value of JUCE_MODAL_LOOPS_PERMITTED has been changed from 1 to 0.

The changes can be grouped in three parts:

asynchronous FileChoosers
-------------------------

All usage of the FileChooser became asynchronous.

The file dialog usage in `askSaveProject` required some additional
refactoring. With the `requestX`-type functions unsaved modifications
will not get lost. The user will be prompted to first save his changes.
With the `uncheckedX`-type functions current modifications will be
dropped silently. The usual flow is:

    requestX -> askForSaveProject -> uncheckedX

The `m_currentFileChooser` members ensure two things:

1. A file dialog is closed when it becomes obsolete, e.g. when the
   player was closed.
2. Each context only keeps open a single dialog. This will for example
   prevent two open dialogs at the same time.

Popup menus
-----------

Those were shown synchronously with a following switch statement that
performed the correct action for the selected menu item. To be
asynchronous the code for each item is now in a callback which is given
to `addItem`. This keeps the item and its code closer and allows showing
the popup asynchronously.

**Potential issue:** The popups have no parent item so they might live
longer than the bound `this`. For that to be destroyed some user
interaction is required but each user interaction closes the popup, so
this should not be a problem.

CrashDumper
-----------

While a crash dump is written on Windows a dialog is shown. This dialog
can no longer block the exception filter so a future was introduced that
gets resolved once the dump file was written.

Source/TrackUi.cpp
@@ -140,30 +140,20 @@ void TrackUi::buttonClicked(juce::Button* button)
     else if (button == &m_editButton)
     {
         juce::PopupMenu m;
-        m.addItem(1, TRANS("edit track"));
-        m.addItem(5, TRANS("delete track"), !m_trackHasFilesCallback(m_track.getTrackIndex()));
-        m.addItem(2, TRANS("open file"));
-        m.addItem(3, TRANS("edit file"), m_track.getTrackConfig().file != juce::File());
-        m.addItem(4, TRANS("remove file"), m_track.getTrackConfig().file != juce::File());
-        m.addSeparator();
-        const int result = m.show();
-
-        switch (result)
-        {
-        case 1:
+        m.addItem(TRANS("edit track"), true, false, [this]() {
             m_editDialog = std::make_unique<TrackEditDialogWindow>(m_track.getName(),
                 m_track.getGain(),
                 std::bind(&Track::setName, &m_track, std::placeholders::_1),
                 std::bind(&Track::setGain, &m_track, std::placeholders::_1));
-            break;
-        case 2:
-            loadFile();
-            break;
-        case 3:
-        {
+        });
+        m.addItem(TRANS("delete track"), !m_trackHasFilesCallback(m_track.getTrackIndex()), false, [this]() {
+            m_removeTrackCallback(&m_track);
+        });
+        m.addItem(TRANS("open file"), true, false, [this]() { loadFile(); });
+        m.addItem(TRANS("edit file"), m_track.getTrackConfig().file != juce::File(), false, [this]() {
             if (!juce::File(m_applicationProperties.getUserSettings()->getValue("audioEditor")).existsAsFile())
             {
-                juce::AlertWindow::showMessageBox(
+                juce::AlertWindow::showMessageBoxAsync(
                     juce::AlertWindow::WarningIcon, TRANS("No audio editor"), TRANS("No audio editor was configured."));
                 return;
             }
@@ -173,7 +163,7 @@ void TrackUi::buttonClicked(juce::Button* button)
             arguments.add(m_track.getTrackConfig().file.getFullPathName());
             if (!process.start(arguments))
             {
-                juce::AlertWindow::showMessageBox(juce::AlertWindow::WarningIcon,
+                juce::AlertWindow::showMessageBoxAsync(juce::AlertWindow::WarningIcon,
                     TRANS("Failed launch audio editor"),
                     TRANS("Failed to start the audio editor."));
                 return;
@@ -181,16 +171,13 @@ void TrackUi::buttonClicked(juce::Button* button)
 
             juce::File file(m_track.getTrackConfig().file);
             m_track.unloadFile();
-            juce::AlertWindow blockDialog(TRANS("Audio editor launched"),
-                TRANS("Modify the file in the audio editor. Click on 'ok' after the file was saved to load it again."),
-                juce::AlertWindow::NoIcon);
-            blockDialog.addButton(TRANS("ok"), 1);
-            blockDialog.runModalLoop();
+            juce::AlertWindow::showMessageBoxAsync(juce::AlertWindow::NoIcon,
+                TRANS("Audio editor launched"),
+                TRANS("Modify the file in the audio editor. Click on 'ok' after the file was saved to load it "
+                      "again."));
             m_track.loadFileIntoTransport(file);
-            break;
-        }
-        case 4:
-        {
+        });
+        m.addItem(TRANS("remove file"), m_track.getTrackConfig().file != juce::File(), false, [this]() {
             int alertResult = juce::AlertWindow::showOkCancelBox(juce::AlertWindow::QuestionIcon,
                 TRANS("Confirm"),
                 TRANS("Do you really want to remove the audio file from this track?"),
@@ -201,12 +188,9 @@ void TrackUi::buttonClicked(juce::Button* button)
 
             if (alertResult == 1)
                 m_track.unloadFile();
-
-            break;
-        }
-        case 5:
-            m_removeTrackCallback(&m_track);
-        }
+        });
+        m.addSeparator();
+        m.showMenuAsync(juce::PopupMenu::Options().withTargetComponent(m_editButton));
     }
 }
 
@@ -282,13 +266,16 @@ void TrackUi::positionChanged(double position)
 
 void TrackUi::loadFile()
 {
-    juce::FileChooser myChooser(TRANS("Please select the audio file you want to load ..."),
+    m_currentFileChooser.emplace(TRANS("Please select the audio file you want to load ..."),
         juce::File(),
         m_track.getAudioFormatManager().getWildcardForAllFormats());
-    if (!myChooser.browseForFileToOpen())
-        return;
 
-    loadFile(juce::File(myChooser.getResult()));
+    m_currentFileChooser->launchAsync(juce::FileBrowserComponent::openMode, [this](const juce::FileChooser& chooser) {
+        if (chooser.getResult() == juce::File())
+            return;
+
+        loadFile(juce::File(chooser.getResult()));
+    });
 }
 
 void TrackUi::loadFile(const juce::File& audioFile)

Source/CDPlayer.cpp
@@ -285,19 +285,9 @@ void CDPlayer::mouseDown(const juce::MouseEvent& event)
         return;
 
     juce::PopupMenu m;
-    m.addItem(1, TRANS("configure channels"));
-    m.addItem(2, TRANS("configure appearance"));
-    const int result = m.show();
-
-    switch (result)
-    {
-    case 1:
-        configureChannels();
-        break;
-    case 2:
-        showEditDialog();
-        break;
-    }
+    m.addItem(TRANS("configure channels"), true, false, [this]() { configureChannels(); });
+    m.addItem(TRANS("configure appearance"), true, false, [this]() { showEditDialog(); });
+    m.showMenuAsync(juce::PopupMenu::Options().withTargetComponent(m_configureButton));
 }
 
 void CDPlayer::setGain(float gain)

Source/CrashDumper.cpp
@@ -4,15 +4,19 @@
 
 LONG CALLBACK unhandledExceptionFilter(EXCEPTION_POINTERS* e)
 {
-    CrashDumper crashDumper(e);
+    std::promise<void> done;
+    auto future = done.get_future();
+    CrashDumper crashDumper(std::move(done), e);
 
-    crashDumper.runThread();
+    crashDumper.launchThread();
+    future.wait();
 
     return EXCEPTION_CONTINUE_SEARCH;
 }
 
-CrashDumper::CrashDumper(EXCEPTION_POINTERS* e)
+CrashDumper::CrashDumper(std::promise<void>&& promise, EXCEPTION_POINTERS* e)
     : ThreadWithProgressWindow(TRANS("Application crashed.\n\nWriting crash dump ..."), true, false)
+    , promise(std::move(promise))
     , e(e)
 {
     setProgress(-1.0);
@@ -76,6 +80,8 @@ void CrashDumper::run()
         NULL);
 
     CloseHandle(dumpFile);
+
+    promise.set_value();
 }
 
 void CrashDumper::init()

Source/CrashDumper.h
@@ -2,6 +2,8 @@
 
 #ifdef _WIN32
 
+#include <future>
+
 #include "juce_gui_basics/juce_gui_basics.h"
 
 // clang-format off
@@ -15,7 +17,7 @@
 class CrashDumper : public juce::ThreadWithProgressWindow
 {
 public:
-    CrashDumper(EXCEPTION_POINTERS* e);
+    CrashDumper(std::promise<void>&& promise, EXCEPTION_POINTERS* e);
 
     static void init();
 
@@ -24,6 +26,7 @@ public:
     virtual void run() override;
 
 private:
+    std::promise<void> promise;
     EXCEPTION_POINTERS* e;
 };
 

Source/JinglePlayerWindow.cpp
@@ -201,55 +201,33 @@ void JinglePlayerWindow::mouseDown(const juce::MouseEvent& event)
     if (event.eventComponent == &m_configureButton)
     {
         juce::PopupMenu m;
-        m.addItem(1, TRANS("load file"));
-        m.addItem(2, TRANS("configure channels"));
-        m.addItem(3, TRANS("configure appearance"));
-        m.addItem(7, TRANS("configure MIDI"));
+        m.addItem(TRANS("load file"), true, false, [this]() { loadFile(); });
+        m.addItem(TRANS("configure channels"), true, false, [this]() { m_configureChannelsCallback(); });
+        m.addItem(TRANS("configure appearance"), true, false, [this]() { m_showEditDialogCallback(); });
+        m.addItem(TRANS("configure MIDI"), true, false, [this]() { m_configureMidiCallback(); });
         m.addSeparator();
-        m.addItem(4, TRANS("Jingle Mode"), true, true);
-        m.addItem(5, TRANS("Multitrack Mode"));
-        m.addItem(6, TRANS("Playlist Mode"));
+        m.addItem(TRANS("Jingle Mode"), true, true, [this]() { m_changePlayerTypeCallback(PlayerType::Jingle); });
+        m.addItem(
+            TRANS("Multitrack Mode"), true, false, [this]() { m_changePlayerTypeCallback(PlayerType::Multitrack); });
+        m.addItem(TRANS("Playlist Mode"), true, false, [this]() { m_changePlayerTypeCallback(PlayerType::Playlist); });
 
-        const int result = m.show();
-
-        switch (result)
-        {
-        case 1:
-            loadFile();
-            break;
-        case 2:
-            m_configureChannelsCallback();
-            break;
-        case 3:
-            m_showEditDialogCallback();
-            break;
-        case 4:
-            m_changePlayerTypeCallback(PlayerType::Jingle);
-            break;
-        case 5:
-            m_changePlayerTypeCallback(PlayerType::Multitrack);
-            break;
-        case 6:
-            m_changePlayerTypeCallback(PlayerType::Playlist);
-            break;
-        case 7:
-            m_configureMidiCallback();
-            break;
-        }
+        m.showMenuAsync(juce::PopupMenu::Options().withTargetComponent(m_configureButton));
     }
 }
 
 void JinglePlayerWindow::loadFile()
 {
-    juce::FileChooser myChooser(TRANS("Please select the audio file you want to load ..."),
+    m_currentFileChooser.emplace(TRANS("Please select the audio file you want to load ..."),
         juce::File(),
         m_formatManager.getWildcardForAllFormats());
 
-    if (!myChooser.browseForFileToOpen())
-        return;
+    m_currentFileChooser->launchAsync(juce::FileBrowserComponent::openMode, [this](const juce::FileChooser& chooser) {
+        if (chooser.getResult() == juce::File())
+            return;
 
-    juce::File audioFile = juce::File(myChooser.getResult());
-    (*m_tracksContainer)[0].loadFileIntoTransport(audioFile);
+        juce::File audioFile = juce::File(chooser.getResult());
+        (*m_tracksContainer)[0].loadFileIntoTransport(audioFile);
+    });
 }
 
 void JinglePlayerWindow::buttonClicked(juce::Button* /*button*/)

Source/JinglePlayerWindow.h
@@ -95,5 +95,7 @@ private:
 
     Waveform m_waveform;
 
+    std::optional<juce::FileChooser> m_currentFileChooser;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(JinglePlayerWindow)
 };

Source/Main.cpp
@@ -120,10 +120,7 @@ public:
 
         void closeButtonPressed()
         {
-            if (!static_cast<MainContentComponent*>(getContentComponent())->askSaveProject())
-                return;
-
-            juce::JUCEApplication::getInstance()->systemRequestedQuit();
+            static_cast<MainContentComponent*>(getContentComponent())->askSaveProject(&MainContentComponent::quit);
         }
 
         void openProject(juce::File projectFile)

Source/MainComponent.cpp
@@ -114,6 +114,11 @@ void MainContentComponent::reconfigSnapToGrid()
     m_multiDocumentPanel->reconfigSnapToGrid(snapToGrid, gridWidth, gridHeight);
 }
 
+void MainContentComponent::quit()
+{
+    juce::JUCEApplication::getInstance()->systemRequestedQuit();
+}
+
 void MainContentComponent::resized()
 {
     auto bounds = getLocalBounds();
@@ -369,10 +374,10 @@ bool MainContentComponent::perform(const juce::ApplicationCommandTarget::Invocat
     switch (info.commandID)
     {
     case projectNew:
-        newProject();
+        requestNewProject();
         break;
     case projectOpen:
-        openProject();
+        requestOpenProject();
         break;
     case projectSave:
         saveProject();
@@ -503,11 +508,13 @@ bool MainContentComponent::perform(const juce::ApplicationCommandTarget::Invocat
     return true;
 }
 
-void MainContentComponent::newProject()
+void MainContentComponent::requestNewProject()
 {
-    if (!askSaveProject())
-        return;
+    askSaveProject(&MainContentComponent::uncheckedNewProject);
+}
 
+void MainContentComponent::uncheckedNewProject()
+{
     getParentComponent()->setName("M*Player");
     m_multiDocumentPanel->closeAllDocuments(true);
     m_projectModified = false;
@@ -521,60 +528,82 @@ void MainContentComponent::openProject(juce::File projectFile)
     readProjectFile();
 }
 
-void MainContentComponent::openProject()
+void MainContentComponent::requestOpenProject()
 {
-    if (!askSaveProject())
-        return;
-
-    juce::FileChooser myChooser(TRANS("Please select the project file you want to load ..."), juce::File(), "*.aupp");
-    if (myChooser.browseForFileToOpen())
-    {
-        openProject(myChooser.getResult());
-    }
+    askSaveProject(&MainContentComponent::uncheckedOpenProject);
 }
 
-bool MainContentComponent::askSaveProject()
+void MainContentComponent::uncheckedOpenProject()
+{
+    m_currentFileChooser.emplace(TRANS("Please select the project file you want to load ..."), juce::File(), "*.aupp");
+
+    m_currentFileChooser->launchAsync(juce::FileBrowserComponent::openMode, [this](const juce::FileChooser& chooser) {
+        if (chooser.getResult() == juce::File())
+            return;
+
+        openProject(chooser.getResult());
+    });
+}
+
+void MainContentComponent::askSaveProject(ContinueCallback callback)
 {
     if (!m_projectModified)
-        return true;
-
-    switch (juce::AlertWindow::showYesNoCancelBox(juce::AlertWindow::QuestionIcon,
-        TRANS("Save project?"),
-        TRANS("Do you want to save the current project?"),
-        TRANS("Yes"),
-        TRANS("No"),
-        TRANS("Cancel"),
-        this))
     {
-    case 1:
-        return saveProject();
-    case 2:
-        return true;
-    default:
-        return false;
+        std::invoke(callback, this);
+        return;
     }
+
+    juce::AlertWindow::showAsync(juce::MessageBoxOptions()
+                                     .withIconType(juce::AlertWindow::QuestionIcon)
+                                     .withTitle(TRANS("Save project?"))
+                                     .withMessage(TRANS("Do you want to save the current project?"))
+                                     .withButton(TRANS("Yes"))
+                                     .withButton(TRANS("No"))
+                                     .withButton(TRANS("Cancel"))
+                                     .withAssociatedComponent(this),
+        [this, callback](int result) {
+            switch (result)
+            {
+            case 1:
+                saveProject(callback);
+                break;
+            case 2:
+                std::invoke(callback, this);
+                break;
+            }
+        });
 }
 
-bool MainContentComponent::saveProject()
+void MainContentComponent::saveProject(std::optional<ContinueCallback> callback)
 {
     if (m_projectFile == juce::File())
-        return saveAsProject();
+    {
+        saveAsProject(callback);
+        return;
+    }
 
     writeProjectFile();
-    return true;
+    if (callback)
+        std::invoke(*callback, this);
 }
 
-bool MainContentComponent::saveAsProject()
+void MainContentComponent::saveAsProject(std::optional<ContinueCallback> callback)
 {
-    juce::FileChooser myChooser(TRANS("Please select the project file you want to save ..."), juce::File(), "*.aupp");
-    if (!myChooser.browseForFileToSave(true))
-        return false;
+    m_currentFileChooser.emplace(TRANS("Please select the project file you want to save ..."), juce::File(), "*.aupp");
 
-    m_projectFile = juce::File(myChooser.getResult());
-    m_recentlyOpenedFiles.addFile(m_projectFile);
-    m_applicationProperties.getUserSettings()->setValue("recentlyUsed", m_recentlyOpenedFiles.toString());
-    writeProjectFile();
-    return true;
+    m_currentFileChooser->launchAsync(
+        juce::FileBrowserComponent::saveMode | juce::FileBrowserComponent::warnAboutOverwriting,
+        [this, callback](const juce::FileChooser& chooser) {
+            if (chooser.getResult() == juce::File())
+                return;
+
+            m_projectFile = juce::File(chooser.getResult());
+            m_recentlyOpenedFiles.addFile(m_projectFile);
+            m_applicationProperties.getUserSettings()->setValue("recentlyUsed", m_recentlyOpenedFiles.toString());
+            writeProjectFile();
+            if (callback)
+                std::invoke(*callback, this);
+        });
 }
 
 void MainContentComponent::readProjectFile()

commit e881d9d84839e4a12c68482c3187d41ed3b89689

Fix dialogs that need to be modal
There are two dialogs which are expected to block and not return
immediately. They are broken since 45bf80fc when the code base was
updated to JUCE 6.1.0.

Refactoring those to be asynchronous isn't trivial. To keep them working
for now `JUCE_MODAL_LOOPS_PERMITTED` is being set to `1`, like it was
before the update.

Additionally, one dialog was wrongfully changed to be async. This was
also reverted back.

CMakeLists.txt
@@ -139,6 +139,7 @@ target_compile_definitions(MStarPlayer PRIVATE
     JUCE_WEB_BROWSER=0
     JUCE_USE_CURL=0 
     JUCE_USE_CDREADER=1
+    JUCE_MODAL_LOOPS_PERMITTED=1
     JUCE_APPLICATION_NAME_STRING="$<TARGET_PROPERTY:MStarPlayer,JUCE_PRODUCT_NAME>"
     JUCE_APPLICATION_VERSION_STRING="$<TARGET_PROPERTY:MStarPlayer,JUCE_VERSION>"
 )

Source/TrackUi.cpp
@@ -181,7 +181,7 @@ void TrackUi::buttonClicked(juce::Button* button)
 
                 juce::File file(m_track.getTrackConfig().file);
                 m_track.unloadFile();
-                juce::AlertWindow::showMessageBoxAsync(juce::AlertWindow::NoIcon,
+                juce::AlertWindow::showMessageBox(juce::AlertWindow::NoIcon,
                     TRANS("Audio editor launched"),
                     TRANS("Modify the file in the audio editor. Click on 'ok' after the file was saved to load it "
                           "again."));